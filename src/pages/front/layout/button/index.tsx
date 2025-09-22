f86e29fc3775e806c5f50c61114318004d457c83
Alexander Smith <hongzeyuan0@gmail.com>
2025-09-22 20:27:31 +0800

    feat: 优化按钮组件，增强可复用性并更新头部布局

--- src/pages/front/layout/button/index.tsx
+++ src/pages/front/layout/button/index.tsx
@@ -1,8 +1,15 @@
 import React from "react";
 
-const Button: React.FC<{ icon: React.ReactNode }> = ({ icon }) => {
+interface Props extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  icon: React.ReactNode;
+}
+
+const Button: React.FC<Props> = ({ icon, className, ...props }) => {
   return (
-    <button className="bg-white flex h-9 w-9 items-center justify-center rounded-full border border-[#E8E8EB] hover:bg-gray-200">
+    <button
+      className={`bg-white flex h-9 w-9 items-center justify-center rounded-full border border-[#E8E8EB] hover:bg-gray-200 ${className}`}
+      {...props}
+    >
       {icon}
     </button>
   );
--- src/pages/front/layout/header/index.tsx
+++ src/pages/front/layout/header/index.tsx
@@ -1,85 +1,140 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { Drawer } from "antd";
 import classNames from "classnames";
 
-import { IconButton, SvgIcon, Iconify } from "@/components/Icon";
+import { Iconify } from "@/components/Icon";
 import Button from "../button";
 
 const Header: React.FC = () => {
-  const [open, setOpen] = useState(false);
+  const [showLeftDrawer, setShowLeftDrawer] = useState(false);
   const navigate = useNavigate();
   //   找到当前激活的链接
   const currentPath = window.location.pathname;
   const isActive = (path: string) => currentPath === path;
 
-  return (
-    <header className="bg-white sticky top-0 z-10 w-full border-b border-[#E4E4E7]">
-      <div className="mx-auto flex h-16 w-full items-center justify-between p-4 sm:p-8 md:max-w-screen-md 2xl:max-w-screen-xl">
-        {/* logo */}
-        <div className="flex items-center gap-4">
-          <Link to="/" className="mr-4 hidden sm:flex sm:items-center">
-            <span className="ml-2 text-base font-semibold !text-black">
-              哲理源
-            </span>
-          </Link>
-        </div>
+  const navClass = (path: string) => {
+    return classNames(
+      "cursor-pointer px-4 py-2 text-sm transition-colors hover:font-semibold hover:text-black",
+      {
+        "text-black font-semibold": isActive(path),
+        "text-gray-600": !isActive(path),
+      },
+    );
+  };
+  const drawerNavClass = (path: string) => {
+    return classNames(
+      "cursor-pointer rounded-xl px-4 py-2 text-sm transition-colors hover:bg-[#F2F2F3]",
+      {
+        "bg-black text-[#fff] hover:bg-[#333]": isActive(path),
+        "hover:bg-[#F2F2F3]": !isActive(path),
+      },
+    );
+  };
 
-        <div className="flex items-center">
-          {/* nav */}
-          <div
-            className="mr-8 hidden items-center gap-3 md:flex"
-            aria-label="主导航"
-          >
+  return (
+    <>
+      <header className="bg-white sticky top-0 z-10 w-full border-b border-[#E4E4E7]">
+        <div className="mx-auto flex h-16 w-full items-center justify-between p-4 sm:p-8 md:max-w-screen-md 2xl:max-w-screen-xl">
+          {/* logo */}
+          <div className="flex items-center gap-4">
+            <Link to="/" className="mr-4 hidden sm:flex sm:items-center">
+              <span className="ml-2 text-base font-semibold !text-black">
+                哲理源
+              </span>
+            </Link>
+            <div className="sm:hidden">
+              <Button
+                icon={<Iconify icon="flowbite:bars-outline" size={20} />}
+                onClick={() => setShowLeftDrawer(true)}
+              />
+            </div>
+          </div>
+
+          <div className="flex items-center">
+            {/* nav */}
             <div
-              className={classNames(
-                "cursor-pointer px-4 py-2 text-sm transition-colors hover:font-semibold hover:text-black",
-                {
-                  "text-black": isActive("/"),
-                  "font-semibold": isActive("/"),
-                  "text-gray-600": !isActive("/"),
-                },
-              )}
+              className="mr-8 hidden items-center gap-3 sm:flex"
+              aria-label="主导航"
+            >
+              <div
+                className={navClass("/home")}
+                onClick={() => navigate("/home")}
+              >
+                首页
+              </div>
+              <div
+                className={navClass("/blog")}
+                onClick={() => navigate("/blog")}
+              >
+                博客
+              </div>
+              <div
+                className={navClass("/about")}
+                onClick={() => navigate("/about")}
+              >
+                关于
+              </div>
+            </div>
+            {/* 按钮 */}
+            <div className="flex items-center gap-3">
+              <Button
+                icon={<Iconify icon="tdesign:logo-github-filled" size={16} />}
+              />
+              <Button
+                icon={<Iconify icon="tdesign:user-setting" size={16} />}
+              />
+            </div>
+          </div>
+        </div>
+      </header>
+      {/* 侧边栏 */}
+      <Drawer
+        placement="left"
+        onClose={() => setShowLeftDrawer(false)}
+        open={showLeftDrawer}
+        closeIcon={false}
+        styles={{
+          header: {
+            display: "none",
+          },
+          body: {
+            padding: 0,
+            overflow: "hidden",
+          },
+        }}
+        width="auto"
+      >
+        <div className="w-[400px]">
+          <div className="px-4 py-4">
+            <div className="text-base font-semibold text-black">哲理源</div>
+            <div className="mt-1 text-sm text-gray-600">
+              努力做一个更好的程序员
+            </div>
+          </div>
+          <div className="mt-16 flex flex-col gap-4">
+            <div
+              className={drawerNavClass("/home")}
               onClick={() => navigate("/")}
             >
               首页
             </div>
             <div
-              className={classNames(
-                "cursor-pointer px-4 py-2 text-sm transition-colors hover:font-semibold hover:text-black",
-                {
-                  "text-black": isActive("/blog"),
-                  "font-semibold": isActive("/blog"),
-                  "text-gray-600": !isActive("/blog"),
-                },
-              )}
+              className={drawerNavClass("/blog")}
               onClick={() => navigate("/blog")}
             >
               博客
             </div>
             <div
-              className={classNames(
-                "cursor-pointer px-4 py-2 text-sm transition-colors hover:font-semibold hover:text-black",
-                {
-                  "text-black": isActive("/about"),
-                  "font-semibold": isActive("/about"),
-                  "text-gray-600": !isActive("/about"),
-                },
-              )}
+              className={drawerNavClass("/about")}
               onClick={() => navigate("/about")}
             >
               关于
             </div>
           </div>
-          {/* 按钮 */}
-          <div className="flex items-center gap-3">
-            <Button
-              icon={<Iconify icon="tdesign:logo-github-filled" size={16} />}
-            />
-            <Button icon={<Iconify icon="tdesign:user-setting" size={16} />} />
-          </div>
         </div>
-      </div>
-    </header>
+      </Drawer>
+    </>
   );
 };
 
