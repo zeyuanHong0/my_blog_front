93b0d0239b5d2de15f4504bfca949034ea2d284a
Alexander Smith <hongzeyuan0@gmail.com>
2025-10-25 22:29:11 +0800

    feat: 添加退出登录功能，支持确认对话框和用户信息显示

--- src/pages/admin/layout/index.tsx
+++ src/pages/admin/layout/index.tsx
@@ -1,4 +1,6 @@
+import { useState } from "react";
 import { Outlet } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { LogOut, User } from "lucide-react";
 
 import { cn } from "@/lib/utils";
@@ -20,66 +22,99 @@ import {
   DropdownMenuTrigger,
   DropdownMenuSeparator,
 } from "@/components/ui/dropdown-menu";
+import ConfirmDialog from "@/components/confirm-dialog";
 import { AppSidebar } from "./app-sidebar";
 
 const AdminLayout = () => {
-  const { userInfo } = useUserStore();
+  const navigate = useNavigate();
+  const { userInfo, userLogout } = useUserStore();
+
+  // 退出登录相关
+  const [isDeleteConfirmOpen, setIsDeleteConfirmOpen] = useState(false);
+  const handleLogout = () => {
+    const res = userLogout();
+    if (res === "success") {
+      navigate("/auth/login");
+    }
+  };
   return (
-    <SidebarProvider>
-      <AppSidebar />
-      <SidebarInset>
-        <header
-          className={cn(
-            "sticky inset-x-0 top-0 z-[5] flex h-16 shrink-0 items-center justify-between gap-2 px-10",
-            "bg-background/50 backdrop-blur",
-          )}
-        >
-          <div className="flex items-center gap-2 px-4">
-            <SidebarTrigger />
-            <Separator
-              orientation="vertical"
-              className={`mr-2 data-[orientation=vertical]:h-4`}
-            />
-          </div>
-          <div className="flex items-center gap-2">
-            <DropdownMenu>
-              <DropdownMenuTrigger asChild>
-                <div>
-                  <Button variant="ghost" size="icon" className="rounded-full">
-                    <User />
-                  </Button>
-                </div>
-              </DropdownMenuTrigger>
-              <DropdownMenuContent
-                className="w-[--radix-dropdown-menu-trigger-width] min-w-56 rounded-lg"
-                side="bottom"
-                align="end"
-                sideOffset={4}
-              >
-                <DropdownMenuLabel className="font-normal">
-                  <div className="flex flex-col gap-1 px-1 py-1.5 text-left text-sm leading-tight">
-                    <span className="truncate font-semibold">
-                      {userInfo?.username}
-                    </span>
-                    <span className="truncate text-xs">{userInfo?.email}</span>
+    <>
+      <SidebarProvider>
+        <AppSidebar />
+        <SidebarInset>
+          <header
+            className={cn(
+              "sticky inset-x-0 top-0 z-[5] flex h-16 shrink-0 items-center justify-between gap-2 px-10",
+              "bg-background/50 backdrop-blur",
+            )}
+          >
+            <div className="flex items-center gap-2 px-4">
+              <SidebarTrigger />
+              <Separator
+                orientation="vertical"
+                className={`mr-2 data-[orientation=vertical]:h-4`}
+              />
+            </div>
+            <div className="flex items-center gap-2">
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                  <div>
+                    <Button
+                      variant="ghost"
+                      size="icon"
+                      className="rounded-full"
+                    >
+                      <User />
+                    </Button>
                   </div>
-                </DropdownMenuLabel>
-                <DropdownMenuSeparator />
-                <DropdownMenuGroup>
-                  <DropdownMenuItem className="cursor-pointer">
-                    <LogOut />
-                    <span>退出登录</span>
-                  </DropdownMenuItem>
-                </DropdownMenuGroup>
-              </DropdownMenuContent>
-            </DropdownMenu>
-          </div>
-        </header>
-        <main>
-          <Outlet />
-        </main>
-      </SidebarInset>
-    </SidebarProvider>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent
+                  className="w-[--radix-dropdown-menu-trigger-width] min-w-56 rounded-lg"
+                  side="bottom"
+                  align="end"
+                  sideOffset={4}
+                >
+                  <DropdownMenuLabel className="font-normal">
+                    <div className="flex flex-col gap-1 px-1 py-1.5 text-left text-sm leading-tight">
+                      <span className="truncate font-semibold">
+                        {userInfo?.username}
+                      </span>
+                      <span className="truncate text-xs">
+                        {userInfo?.email}
+                      </span>
+                    </div>
+                  </DropdownMenuLabel>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuGroup>
+                    <DropdownMenuItem
+                      className="cursor-pointer"
+                      onClick={() => setIsDeleteConfirmOpen(true)}
+                    >
+                      <LogOut />
+                      <span>退出登录</span>
+                    </DropdownMenuItem>
+                  </DropdownMenuGroup>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </div>
+          </header>
+          <main>
+            <Outlet />
+          </main>
+        </SidebarInset>
+      </SidebarProvider>
+      {/* 退出登录提示弹窗 */}
+      <ConfirmDialog
+        cancelBtnText="取消"
+        confirmBtnText="确认"
+        title="退出登录"
+        description={`确定要退出登录吗？`}
+        onCancel={() => setIsDeleteConfirmOpen(false)}
+        onConfirm={handleLogout}
+        isOpen={isDeleteConfirmOpen}
+        onOpenChange={setIsDeleteConfirmOpen}
+      />
+    </>
   );
 };
 
