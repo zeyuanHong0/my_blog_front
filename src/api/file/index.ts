05448ef6b2f38fad2824b19850ffb42c459ff946
Alexander Smith <hongzeyuan0@gmail.com>
2025-10-27 21:01:52 +0800

    fix: 修正文件上传API路径，确保文件上传功能正常

--- src/api/file/index.ts
+++ src/api/file/index.ts
@@ -1,12 +1,12 @@
-import request from '@/utils/axios'
+import request from "@/utils/axios";
 
 enum API {
-  UPLOAD_URL = '/admin/product/fileUpload',
+  UPLOAD_URL = "/upload/file",
 }
 
 // 上传文件
 export const fetchUploadFile = (file: File) => {
-  const formData = new FormData()
-  formData.append('file', file)
-  return request.post<any, any>(API.UPLOAD_URL, formData)
-}
+  const formData = new FormData();
+  formData.append("file", file);
+  return request.post<any, any>(API.UPLOAD_URL, formData);
+};
