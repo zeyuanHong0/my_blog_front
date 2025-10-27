93bfa0ddb067e73c70565c087415143d7b118ac2
Alexander Smith <hongzeyuan0@gmail.com>
2025-10-27 21:01:58 +0800

    feat: 添加图片上传功能，优化上传处理逻辑并显示错误提示

--- src/components/bytemd/editor.tsx
+++ src/components/bytemd/editor.tsx
@@ -1,15 +1,8 @@
 import { Editor, type EditorProps } from "@bytemd/react";
 import zh_Hans from "bytemd/locales/zh_Hans.json";
+import { fetchUploadFile } from "@/api/file";
 
-
-// import {
-//   hideToast,
-//   showErrorToast,
-//   showLoadingToast,
-//   showSuccessToast,
-// } from "@/components/toast";
-
-// import { uploadFile } from "@/features/upload";
+import { showErrorToast } from "@/components/toast";
 
 import { plugins } from "./config";
 
@@ -24,6 +17,26 @@ export const BytemdEditor = ({
   setContent,
   editorProps,
 }: BytemdEditorProps) => {
+  // 处理图片上传
+  const handleUploadImages = async (files: File[]) => {
+    const uploadPromises = files.map(async (file) => {
+      try {
+        const res: any = await fetchUploadFile(file);
+        return {
+          url: res.data?.fileUrl || "",
+          title: res.data?.filename || file.name,
+        };
+      } catch (error: any) {
+        showErrorToast(error.message || `上传${file.name}图片失败`);
+        return {
+          url: "",
+          title: file.name,
+        };
+      }
+    });
+    return Promise.all(uploadPromises);
+  };
+
   return (
     <Editor
       value={body ?? ""}
@@ -33,6 +46,7 @@ export const BytemdEditor = ({
         setContent(v);
       }}
       locale={zh_Hans}
+      uploadImages={handleUploadImages}
       editorConfig={{
         ...editorProps,
       }}
