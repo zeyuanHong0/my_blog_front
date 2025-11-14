a5ea9a181aedc413a3e06318363f154598fa7224
Alexander Smith <hongzeyuan0@gmail.com>
2025-11-14 22:10:23 +0800

    feat: 优化博客列表项组件，添加标签图标并格式化日期显示

--- src/pages/front/blog/list-item/index.tsx
+++ src/pages/front/blog/list-item/index.tsx
@@ -1,6 +1,9 @@
-import { Link } from "react-router-dom";
+import { Calendar } from "lucide-react";
+import dayjs from "dayjs";
 
 import { cn } from "@/lib/utils";
+
+import { SvgIcon } from "@/components/Icon";
 
 const BlogListItem = ({ blog }: { blog: any }) => {
   return (
@@ -13,9 +16,9 @@ const BlogListItem = ({ blog }: { blog: any }) => {
       {/* 标签 */}
       <ul className="mb-1 flex space-x-4 text-xs font-medium">
         {blog.tags.map((tag) => (
-          <li key={tag.id} className="flex items-center">
-            <span className="mr-1">#&nbsp;{tag.name}</span>
-            {/* tagIcon */}
+          <li key={tag.id} className="flex items-center gap-1">
+            <SvgIcon icon={tag.icon} className="h-4 w-4" />
+            <span className="mr-1">{tag.name}</span>
           </li>
         ))}
       </ul>
@@ -23,12 +26,13 @@ const BlogListItem = ({ blog }: { blog: any }) => {
       <h4 className="mb-2 line-clamp-1 text-xl font-medium">{blog.title}</h4>
       {/* 描述 */}
       <p className="text-muted-foreground mb-3 line-clamp-2 text-sm">
-        这个是博客描述，这个是博客描述，这个是博客描述，这个是博客描述，这个是博客描述，这个是博客描述，这个是博客描述，这个是博客描述，这个是博客描述，这个是博客描述，这个是博客描述，这个是博客描述，这个是博客描述，这个是博客描述，这个是博客描述，这个是博客描述，这个是博客描述，这个是博客描述，这个是博客描述，这个是博客描述。
+        {blog.description}
       </p>
       {/* 日期 */}
-      <div className="text-muted-foreground flex items-center space-x-2 text-xs">
+      <div className="text-muted-foreground flex items-center gap-1 text-xs">
         {/* 图标 */}
-        05月10，2025
+        <Calendar className="h-4 w-4" />
+        {dayjs(blog.createTime).format("MM月DD，YYYY")}
       </div>
     </div>
   );
