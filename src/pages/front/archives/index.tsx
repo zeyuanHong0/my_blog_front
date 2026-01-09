e24f5713f1335b00b675e586e1ab85bb6b0d78af
Alexander Smith <hongzeyuan0@gmail.com>
2026-01-09 17:15:54 +0800

    refactor(archives): 优化归档组件，移除测试数据并整理代码结构

--- src/pages/front/archives/index.tsx
+++ src/pages/front/archives/index.tsx
@@ -1,75 +1,38 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import dayjs from "dayjs";
 import { BookText } from "lucide-react";
 
+import { useDocumentTitle } from "@/hooks/useDocumentTitle";
 import { fetchBlogArchives } from "@/api/blog";
 
-const Archives = () => {
-  const [total, setTotal] = useState(0);
-  const list = [
-    {
-      id: 1,
-      title: "测试博客1",
-      createTime: "2026-01-01",
-    },
-    {
-      id: 2,
-      title: "测试博客2",
-      createTime: "2025-10-02",
-    },
-    {
-      id: 3,
-      title: "测试博客3",
-      createTime: "2025-05-03",
-    },
-    {
-      id: 4,
-      title: "测试博客4",
-      createTime: "2025-01-04",
-    },
-  ];
-  const groupedList = [
-    {
-      year: 2026,
-      total: 1,
-      months: [
-        {
-          month: 1,
-          blogs: [
-            {
-              id: 1,
-              title: "测试博客1",
-              createTime: "2026-01-01",
-            },
-          ],
-        },
-      ],
-    },
-    {
-      year: 2025,
-      total: 3,
-      months: [
-        {
-          month: 10,
-          blogs: [{ id: 2, title: "测试博客2", createTime: "2025-10-02" }],
-        },
-        {
-          month: 5,
-          blogs: [{ id: 3, title: "测试博客3", createTime: "2025-05-03" }],
-        },
-        {
-          month: 1,
-          blogs: [{ id: 4, title: "测试博客4", createTime: "2025-01-04" }],
-        },
-      ],
-    },
-  ];
+type Blog = {
+  id: string;
+  title: string;
+  createTime: string;
+};
 
+type YearArchive = {
+  year: number;
+  total: number;
+  months: {
+    month: number;
+    blogs: Blog[];
+  }[];
+};
+
+type ArchivesMap = Record<number, YearArchive>;
+
+const Archives = () => {
+  useDocumentTitle("归档");
+  const [total, setTotal] = useState(0);
+  const [groupedArchivesList, setGroupedArchivesList] = useState<YearArchive[]>(
+    [],
+  );
   // 处理归档结构
-  const groupArchives = (list: any[]) => {
-    const archivesMap = {};
-    list.map((item: any) => {
+  const groupArchives = (list: Blog[]) => {
+    const archivesMap: ArchivesMap = {};
+    list.forEach((item: Blog) => {
       const year = dayjs(item.createTime).year();
       const month = dayjs(item.createTime).month() + 1;
       if (!archivesMap[year]) {
@@ -88,16 +51,17 @@ const Archives = () => {
       }
       archivesMap[year].months[month].blogs.push(item);
     });
-    console.log(Object.values(archivesMap));
+    // console.log(Object.values(archivesMap));
     // =>数组，排序
-    // Object.values(archivesMap)
-    //   .sort((a, b) => b.year - a.year)
-    //   .map((item) => {
-    //     return {
-    //       ...item,
-    //       months: Objectitem.months.sort((a, b) => b.month - a.month),
-    //     };
-    //   });
+    const groupedList = Object.values(archivesMap)
+      .sort((a, b) => b.year - a.year)
+      .map((item) => {
+        return {
+          ...item,
+          months: Object.values(item.months).sort((a, b) => b.month - a.month),
+        };
+      });
+    setGroupedArchivesList(groupedList);
   };
   useEffect(() => {
     const fetchData = async () => {
@@ -116,60 +80,40 @@ const Archives = () => {
         继续努力！
       </p>
       {/* 博客列表 */}
-      <div className="mt-14">
-        <h2 className="mb-4 text-2xl font-bold">
-          <span>2026</span>
-          <span className="text-muted-foreground ml-4 text-sm font-normal">
-            共3篇博客
-          </span>
-        </h2>
-        <div>
-          <h3 className="text-xl font-bold">1月</h3>
-          <ul className="my-4 list-inside">
-            <li className="text-muted-foreground hover:text-primary mb-4 flex items-center">
-              <BookText size={16} className="mr-2" />
-              <Link
-                className="flex flex-1 items-center justify-between text-sm"
-                to="/blog/1"
-              >
-                <div className="mr-4 line-clamp-1 flex-1 break-all">
-                  测试博客1
-                </div>
-                <div className="text-muted-foreground w-20 text-xs">
-                  1月 06, 2026
-                </div>
-              </Link>
-            </li>
-            <li className="text-muted-foreground hover:text-primary mb-4 flex items-center">
-              <BookText size={16} className="mr-2" />
-              <Link
-                className="flex flex-1 items-center justify-between text-sm"
-                to="/blog/2"
-              >
-                <div className="mr-4 line-clamp-1 flex-1 break-all">
-                  测试博客2
-                </div>
-                <div className="text-muted-foreground w-20 text-xs">
-                  1月 06, 2026
-                </div>
-              </Link>
-            </li>
-            <li className="text-muted-foreground hover:text-primary mb-4 flex items-center">
-              <BookText size={16} className="mr-2" />
-              <Link
-                className="flex flex-1 items-center justify-between text-sm"
-                to="/blog/3"
-              >
-                <div className="mr-4 line-clamp-1 flex-1 break-all">
-                  测试博客3
-                </div>
-                <div className="text-muted-foreground w-20 text-xs">
-                  1月 03, 2026
-                </div>
-              </Link>
-            </li>
-          </ul>
-        </div>
+      <div className="mt-12">
+        {groupedArchivesList.map((yearArchive) => (
+          <React.Fragment key={yearArchive.year}>
+            <h2 className="mb-4 text-2xl font-bold">
+              <span>{yearArchive.year}</span>
+              <span className="text-muted-foreground ml-4 text-sm font-normal">
+                共{yearArchive.total}篇博客
+              </span>
+            </h2>
+            {yearArchive.months.map((monthArchive) => (
+              <div>
+                <h3 className="text-xl font-bold">{monthArchive.month}月</h3>
+                <ul className="my-4 list-inside">
+                  {monthArchive.blogs.map((blog) => (
+                    <li className="text-muted-foreground hover:text-primary mb-4 flex items-center">
+                      <BookText size={16} className="mr-2" />
+                      <Link
+                        className="flex flex-1 items-center justify-between text-sm"
+                        to={`/blog/${blog.id}`}
+                      >
+                        <div className="mr-4 line-clamp-1 flex-1 break-all">
+                          {blog.title}
+                        </div>
+                        <div className="text-muted-foreground w-20 text-xs">
+                          {dayjs(blog.createTime).format("M月 DD, YYYY")}
+                        </div>
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            ))}
+          </React.Fragment>
+        ))}
       </div>
     </div>
   );
